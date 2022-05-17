2b7fd3ab4ca719f684c98f5f60e63b35104cf5be
HassanTAli <hassantarekha@gmail.com>
2022-05-17 11:57:19 +0200

    Controllers and connect controllers functions

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,38 @@
+// @desc    get Goals
+// @route   GET /api/goals
+// @access  private
+
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "Get Goals" });
+};
+
+// @desc    set Goal
+// @route   POST /api/goals
+// @access  private
+
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "Set Goals" });
+};
+
+// @desc    update Goal
+// @route   PUT /api/goals/:id
+// @access  private
+
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+
+// @desc    delete Goals
+// @route   /api/goals/:id
+// @access  private
+
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
