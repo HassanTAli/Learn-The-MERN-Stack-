c398254f5e5b0312711da2f60056f478a9ed03aa
HassanTAli <hassantarekha@gmail.com>
2022-05-18 14:07:34 +0200

    Authenticate User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -49,7 +49,21 @@ const registerUser = expressHandler(async (req, res) => {
 //@route   POST /api/users/login
 //@access  public
 const loginUser = expressHandler(async (req, res) => {
-  res.json({ message: 'Login User' })
+  const { email, password } = req.body
+
+  // Check for user email
+  const user = await User.findOne({ email })
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid credentials')
+  }
 })
 
 //@desc    Get user data
