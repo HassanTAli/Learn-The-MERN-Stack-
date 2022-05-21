d2bcd4df5b9deccc6c267c8a636b7cb05a052fb1
HassanTAli <hassantarekha@gmail.com>
2022-05-21 09:07:47 +0200

    Frontend UI

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from '../Components/Header'
 import DashBoard from '../Pages/DashBoard'
 import Login from '../Pages/Login'
 import Register from '../Pages/Register'
@@ -10,6 +11,7 @@ const App = () => {
         <>
             <Router>
                 <div className="container">
+                    <Header />
                     <Routes>
                         <Route path='/' element={<DashBoard />} />
                         <Route path='/login' element={<Login />} />
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,7 +1,59 @@
+import { useState } from 'react';
+import { FaSignInAlt } from 'react-icons/fa'
+
 const Login = () => {
-    return (
-        <div>Login</div>
-    )
+    const [fromData, setFromData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email, password } = fromData
+
+    const onChange = (e) => {
+        setFromData(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+    const onSubmit = (e) => { e.preventDefault() }
+
+    return <>
+        <section className="heading">
+            <h1>
+                <FaSignInAlt /> Login
+            </h1>
+            <p>Login and start setting Goals. </p>
+        </section>
+        <section className="form">
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input
+                        type='email'
+                        className='form-control'
+                        id='email'
+                        name='email'
+                        value={email}
+                        placeholder="Enter your email"
+                        onChange={onChange}
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type='password'
+                        className='form-control'
+                        id='password'
+                        name='password'
+                        value={password}
+                        placeholder="Enter password"
+                        onChange={onChange}
+                    />
+                </div>
+                <div className='from-group'>
+                    <button type="submit" className='btn btn-block'>Login</button>
+                </div>
+            </form>
+        </section>
+    </>
 }
 
 export default Login
