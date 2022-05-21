a553da3d0a475f69e8cab920254526e2f32a9f9e
HassanTAli <hassantarekha@gmail.com>
2022-05-21 08:11:39 +0200

    bluid Component folder & React Router Setup

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import DashBoard from '../Components/DashBoard'
+import Login from '../Components/Login'
+import Register from '../Components/Register'
+
 import './App.css'
 
 const App = () => {
     return (
-        <div>App</div>
+        <>
+            <Router>
+                <div className="container">
+                    <Routes>
+                        <Route path='/' element={<DashBoard />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/register' element={<Register />} />
+                    </Routes>
+                </div>
+            </Router>
+        </>
     )
 }
 
