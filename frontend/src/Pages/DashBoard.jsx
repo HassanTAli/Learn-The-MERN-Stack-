5e741a30806ec5c802d54c64c3b9b1dcd8d5bfb6
HassanTAli <hassantarekha@gmail.com>
2022-05-22 16:24:43 +0200

    restrict DashBoard

--- frontend/src/Pages/DashBoard.jsx
+++ frontend/src/Pages/DashBoard.jsx
@@ -1,4 +1,17 @@
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+
 const DashBoard = () => {
+    const navigate = useNavigate()
+    const { user } = useSelector(state => state.auth)
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/login')
+        }
+    }, [user, navigate])
+
     return (
         <div>DashBoard</div>
     )
